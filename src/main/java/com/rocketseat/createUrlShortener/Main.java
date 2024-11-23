9c292da77f198cb5bfb8dce9640d2836d49ab9a4
vicenttcarvalho <vicentecleyton@gmail.com>
2024-11-22 21:46:40 -0300

    Timestamp Fix

--- src/main/java/com/rocketseat/createUrlShortener/Main.java
+++ src/main/java/com/rocketseat/createUrlShortener/Main.java
@@ -30,7 +30,7 @@ public class Main implements RequestHandler<Map<String, Object>, Map<String, Str
 
         String originalUrl = bodyMap.get("originalUrl");
         String expirationTime = bodyMap.get("expirationTime");
-        long expirationTimeInSeconds = Long.parseLong(expirationTime) * 3600;
+        long expirationTimeInSeconds = Long.parseLong(expirationTime);
 
         String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
 
